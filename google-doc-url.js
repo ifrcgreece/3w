8ee928652eb0ab4972ac120a625a8f39c968f25e
yorgossotiriadis <yorgossotiriadis@gmail.com>
2017-06-20 21:46:08 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1jqMg_XL_bTviPH0F1lkHIPgSg17AULVGy9QOdbd2VY4/edit#gid=0';
